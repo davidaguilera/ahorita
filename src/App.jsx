8c44e5f373eefa188f0bd6992dadff9a36124e50
geektor <geektor@2806-103e-000e-4f95-0000-0000-0000-0002.ipv6.infinitum.net.mx>
2025-08-10 13:33:31 -0600

    Initial setup with react, react router, tailwindv4 and axios

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+
+  return (
+    <>
+      <p className="title is-size-1 text-green-600 has-text-centered">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+
